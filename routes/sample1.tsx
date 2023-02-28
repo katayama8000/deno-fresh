dbb4708b62e17ca06e75fb9d6e74617265bf2131
katayama8000 <tattu.0310@gmail.com>
2023-02-28 10:17:45 +0900

    www

---
+++ routes/sample1.tsx
@@ -0,0 +1,13 @@
+export default function Sample1() {
+  return (
+    <div>
+      <button
+        onClick={() => {
+          console.log('click');
+        }}
+      >
+        click
+      </button>
+    </div>
+  );
+}
