ffe31b121843bf98891000c6a3c16c5c8d224d7c
katayama8000 <tattu.0310@gmail.com>
2023-05-01 00:56:42 +0900

    update

--- routes/api/puppeteer.ts
+++ routes/api/puppeteer.ts
@@ -7,7 +7,7 @@ export const handler = async (_req: Request, _ctx: HandlerContext) => {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
     await page.goto('https://example.com');
-    await page.screenshot({ path: 'example.png' });
+    // await page.screenshot({ path: 'example.png' });
 
     await browser.close();
     return new Response('success');
