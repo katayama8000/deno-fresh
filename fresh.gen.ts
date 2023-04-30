78693a55d338f681fcea69387ff7b82ad35cccdc
katayama8000 <tattu.0310@gmail.com>
2023-05-01 00:24:14 +0900

    update

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -7,6 +7,7 @@ import * as $0 from "./routes/[name].tsx";
 import * as $1 from "./routes/api/joke.ts";
 import * as $2 from "./routes/index.tsx";
 import * as $3 from "./routes/sample1.tsx";
+import * as $4 from "./routes/sample2.tsx";
 import * as $$0 from "./islands/Counter.tsx";
 
 const manifest = {
@@ -15,6 +16,7 @@ const manifest = {
     "./routes/api/joke.ts": $1,
     "./routes/index.tsx": $2,
     "./routes/sample1.tsx": $3,
+    "./routes/sample2.tsx": $4,
   },
   islands: {
     "./islands/Counter.tsx": $$0,
---
+++ routes/sample2.tsx
@@ -0,0 +1,13 @@
+export default function Sample2() {
+  return (
+    <div>
+      <button
+        onClick={() => {
+          console.log('click');
+        }}
+      >
+        click
+      </button>
+    </div>
+  );
+}
